2b288d964d620810e18f11924afab428a1d0bea7
Dima Savkov <dimsaw85@gmail.com>
2022-02-15 13:59:21 +0200

    finish

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+
 import ContactsList from './componets/ContactsList';
 import ContactForm from './componets/ContactForm';
 import Filter from './componets/Filter';
@@ -6,16 +7,45 @@ import Container from './componets/Container';
 import s from './App.module.css';
 import shortid from 'shortid';
 
+import Notiflix from 'notiflix';
+Notiflix.Notify.init({
+  width: '380px',
+  position: 'center-top',
+  failure: {
+    background: '#00bfff',
+    textColor: '#fff',
+  },
+});
+
 class App extends Component {
   state = {
     contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
     filter: '',
   };
+
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const nextContacts = this.state.contacts;
+    const prevContacts = prevState.contacts;
+
+    if (nextContacts !== prevContacts) {
+      console.log('add');
+      localStorage.setItem('contacts', JSON.stringify(nextContacts));
+    }
+  }
 
   formSubmitHandler = ({ name, number }) => {
     const newContact = {
@@ -29,7 +59,7 @@ class App extends Component {
         contact => contact.name.toLowerCase() === name.toLowerCase(),
       )
     ) {
-      return alert(`${name} is alredy in contacts`);
+      return Notiflix.Notify.failure(`${name} is alredy in contacts`);
     }
 
     this.setState(prevState => ({
@@ -62,57 +92,27 @@ class App extends Component {
     const emptyContacts = this.state.contacts.length;
 
     return (
-      <Container>
-        <h1 className={s.text}>Phonebook</h1>
-        <ContactForm onSubmit={this.formSubmitHandler} />
+      <>
+        <Container>
+          <h1 className={s.text}>Phonebook</h1>
+          <ContactForm onSubmit={this.formSubmitHandler} />
 
-        <div>
-          <h3 className={s.contacts}>Contacts</h3>
-          <Filter value={filter} onChange={this.changeFilter} />
-          {emptyContacts > 0 ? (
-            <ContactsList
-              contacts={visibleContact}
-              onDeleteContact={this.deleteContact}
-            />
-          ) : (
-            <h4>Phonebook is empty</h4>
-          )}
-        </div>
-      </Container>
+          <div>
+            <h3 className={s.contacts}>Contacts</h3>
+            <Filter value={filter} onChange={this.changeFilter} />
+            {emptyContacts > 0 ? (
+              <ContactsList
+                contacts={visibleContact}
+                onDeleteContact={this.deleteContact}
+              />
+            ) : (
+              <h4>Phonebook is empty</h4>
+            )}
+          </div>
+        </Container>
+      </>
     );
   }
 }
 
 export default App;
-
-//   componentDidMount() {
-//     // console.log('App componentDidMount');
-
-//     const contacts = localStorage.getItem('contacts');
-//     const parsedContacts = JSON.parse(contacts);
-
-//     if (parsedContacts) {
-//       this.setState({ contacts: parsedContacts });
-//     }
-//   }
-
-//   componentDidUpdate(prevProps, prevState) {
-//     // console.log('App componentDidUpdate');
-
-//     const nextContacts = this.state.contacts;
-//     const prevContacts = prevState.contacts;
-
-//     if (nextContacts !== prevContacts) {
-//       console.log('add');
-//       localStorage.setItem('contacts', JSON.stringify(nextContacts));
-//     }
-
-//     if (
-//       nextContacts.length > prevContacts.length &&
-//       prevContacts.length !== 0
-//     ) {
-//       this.getVisibleContacts();
-//     }
-//   }
-
-//
